1a39a49b674d32d251ded2d8ccd6ca0a6adef964
Nicola Moretto <nicola@tyk.li>
2016-11-24 22:17:17 +0100

    Added model interface

---
+++ src/Model/ModelInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NicolaMoretto\TheTVDB\Model;
+
+/**
+ *
+ * @author Nicola Moretto <n.moretto@nicolamoretto.eu>
+ */
+interface ModelInterface {
+	
+	/**
+	 * Map a TheTVDB entity into a class instance
+	 *
+	 * @param array $tvdbData
+	 *        	Associative array containing all information about a TheTVDB entity
+	 * @return ModelInterface Return an instance of the class representing the given TheTVDB entity
+	 */
+	public function createFrom($tvdbData): ModelInterface;
+}
